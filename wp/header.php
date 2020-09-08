30de5ee26a5bd9cae86aa530340f35ae00808436
idemac1902 <hiroyuki_toumoto@idee-kobe.com>
2020-09-08 11:45:38 +0900

    headerとfooterを共通化しました

---
+++ wp/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="JA兵庫西">
+    <meta name="description" content="JA兵庫西の広報誌にっしぃ広場のWEBサイトです">
+    <title>にっしぃ広場WEB</title>
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="icon" href="img/icon.png" sizes="16x16" type="image/png">
+  </head>
+  <body>
+    <div class="main">
+      <!-- seoのため非表示-->
+      <h1>JA兵庫西</h1>
+      <header>
+        <div class="l-header">
+          <ul class="menu">
+            <li><a href="index.html"><img src="img/logo.png"></a></li>
+            <li>
+              <h2 class="c-title-main">にっしぃひろば</h2>
+            </li>
+            <li>
+              <div class="search_container">
+                <input type="text" placeholder="検索はこちら">
+                <input type="submit" value="">
+              </div>
+              <div class="l-header-icon"><a href=""><i class="fab fa-instagram-square fa-2x l-header-icon__start"></i></a><a href=""><i class="fab fa-facebook-square fa-2x"></i></a></div>
+            </li>
+          </ul>
+        </div>
+        <div class="c-title-main-top__under"><span>YOGONISHI  AGRICULTURAL  COOPERATIVE</span></div>
+      </header>
