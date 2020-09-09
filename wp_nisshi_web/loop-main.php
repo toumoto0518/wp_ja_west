e10823239cfdc22de792a341d9eac91146b35adf
idemac1902 <hiroyuki_toumoto@idee-kobe.com>
2020-09-09 12:51:50 +0900

    記事ブロックの余白を調整しました

--- wp_nisshi_web/loop-main.php
+++ wp_nisshi_web/loop-main.php
@@ -4,14 +4,14 @@ if( have_posts() ) :
 ?>
 
   <article id="post-<?php the_ID(); ?>" <?php post_class('l-article-column2__list')?>>
-    <a class="l-article-column2__list" href="<?php the_permalink(); ?>">
+    <a class="l-article-column2__list_a" href="<?php the_permalink(); ?>">
       <?php the_post_thumbnail('thumbnail'); ?>
-      <h4 class="l-article-column2__list__title"><?php the_title(); ?></h4>
-      <div class="l-article-column2__list__tag red">
-      <time datetime="<?php the_time('Y-m-d'); ?>" class="<?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->slug; } ?>"><?php the_time('Y年m日d日'); ?></time>
-      <div class="<?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->slug; } ?>"><?php the_category(); ?></div>
+      <h4 class="l-article-column2__list__title u-mgn--t8px"><?php the_title(); ?></h4>
+      <div class="l-article-column2__list__tag u-mgn--t8px">
+        <time datetime="<?php the_time('Y-m-d'); ?>" class="<?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->slug; } ?>"><?php the_time('Y年m日d日'); ?></time>
+        <div class="<?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->slug; } ?>"><?php the_category(); ?></div>
       </div>
-      <p><?php the_excerpt(); ?></p>
+      <p class="u-mgn--t8px"><?php the_excerpt(); ?></p>
     </a>
   </article>
 
