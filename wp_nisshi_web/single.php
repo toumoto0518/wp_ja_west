d3afac2ce612f052c8ef66eccee7a845c42b348e
idemac1902 <hiroyuki_toumoto@idee-kobe.com>
2020-09-17 11:03:29 +0900

    CSS修正

--- wp_nisshi_web/single.php
+++ wp_nisshi_web/single.php
@@ -4,19 +4,23 @@
       <header>
         <div class="l-header">
           <ul class="menu">
-            <li><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png"></a></li>
-            <li>
-              <h2 class="c-title-main">にっしぃひろば</h2>
+            <!-- <li><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png"></a></li> -->
+            <li class="l-header__menu__li">
+              <h2 class="c-title-main"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/top-title01.png" alt=""></a></h2>
             </li>
             <li>
               <div class="search_container">
                 <?php get_search_form(); ?>
               </div>
-              <div class="l-header-icon"><a href="https://www.instagram.com/ja_hyogo/"><i class="fab fa-instagram-square fa-2x l-header-icon__start"></i></a><a href="https://www.facebook.com/pages/JA%E5%85%B5%E5%BA%AB%E8%A5%BF/112179886000623"><i class="fab fa-facebook-square fa-2x"></i></a></div>
+              <div class="l-header-icon">
+                <a href="http://www.ja-hyogonishi.com/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png"></a>
+                <a href="https://www.instagram.com/ja_hyogo/" target="_blank"><i class="fab fa-instagram-square fa-2x l-header-icon__start"></i></a>
+                <a href="https://www.facebook.com/pages/JA%E5%85%B5%E5%BA%AB%E8%A5%BF/112179886000623" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a>
+              </div>
             </li>
           </ul>
         </div>
-        <div class="c-title-main-top__under"><span>HYOGONISHI  AGRICULTURAL  COOPERATIVE</span></div>
+        <!-- <div class="c-title-main-top__under"><span>HYOGONISHI  AGRICULTURAL  COOPERATIVE</span></div> -->
       </header>
 
       <?php
